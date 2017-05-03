98b69ded0590d237c463a349ba799ca81b73d56e
JAPA <ja-pa@users.noreply.github.com>
2017-05-03 13:56:25 +0200

    update_package: add youtube-dl update fcn

--- scripts/update_package.sh
+++ scripts/update_package.sh
@@ -2,6 +2,10 @@
 
 PKG_DIR=$(pwd)/..
 
+get_latest_release_tag() {
+	curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+}
+
 update_knot_resolver() {
 	last_hash=$(git ls-remote https://gitlab.labs.nic.cz/knot/resolver.git|grep "refs/heads/master"|awk '{print $1}')
 	sed -i "s|PKG_VERSION:=.*|PKG_VERSION:=${last_hash}|" $PKG_DIR/knot-resolver/Makefile
@@ -14,18 +18,16 @@ update_knot_resolver_forward() {
 	echo knot-resolver-forward updated
 }
 
-
 update_knot() {
-last_hash=aaaaaaaaaaaaaa
-echo "NI"
+	echo "NI"
 }
 
 update_youtube_dl() {
-echo "NI"
-
+	last_tag=$(get_latest_release_tag "rg3/youtube-dl")
+	sed -i "s|PKG_VERSION:=.*|PKG_VERSION:=${last_tag}|" $PKG_DIR/youtube-dl/Makefile
 }
-
 
 
 update_knot_resolver_forward
 update_knot_resolver
+update_youtube_dl
